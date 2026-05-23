8ccd68e7d04fd55ad8016975d70031355e3de629
Alok Kumar <rajalok957641@gmail.com>
2026-05-23 09:58:19 +0530

    track order page

--- Swigo/backend/controller/user.controller.js
+++ Swigo/backend/controller/user.controller.js
@@ -5,16 +5,37 @@ const User = require("../models/user/usermodel.js")
 // controller/user.controller.js
 const getcurruser = async (req, res) => {
     try {
+
         const userId = req.userId;
-        if (!userId) return res.status(401).json({ message: "UserId not found" });
+
+        if (!userId) {
+            return res.status(401).json({
+                success:false,
+                message:"UserId not found"
+            });
+        }
 
         const user = await User.findById(userId).select("-password");
-        if (!user) return res.status(401).json({ message: "User not found" });
 
-        // 🚨 CULPRIT: {user} ki jagah sirf user bhejo!
-        return res.status(200).json(user);
+        if (!user) {
+            return res.status(401).json({
+                success:false,
+                message:"User not found"
+            });
+        }
+
+        return res.status(200).json({
+            success:true,
+            user
+        });
+
     } catch (error) {
-        return res.status(500).json({ message: "Get current user error" });
+
+        return res.status(500).json({
+            success:false,
+            message:"Get current user error"
+        });
+
     }
 }
 
