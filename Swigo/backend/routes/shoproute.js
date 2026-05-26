33e3d4dcc8cad0d3564e62a08db86741d753a964
Alok Kumar <rajalok957641@gmail.com>
2026-05-26 09:23:33 +0530

    perf: optimize app loading with lazy loading and caching

--- Swigo/backend/routes/shoproute.js
+++ Swigo/backend/routes/shoproute.js
@@ -4,16 +4,13 @@ const { isAuth } = require("../middelwear/isAuth.js");
 const Upload = require("../middelwear/multer.js"); 
 const { CreateAndEditShop, getMyShop, getShopByCity } = require("../controller/shop.controller.js");
 
-shoprouter.post("/CreateAndEditShop", isAuth, (req, res, next) => {
-    console.log("CreateAndEditShop Route Triggered");
-    next();
-}, Upload.single("image"), CreateAndEditShop);
+// ✅ CREATE/EDIT SHOP
+shoprouter.post("/CreateAndEditShop", isAuth, Upload.single("image"), CreateAndEditShop);
 
+// ✅ GET MY SHOP (YEH ADD KARO - MISSING THA)
+shoprouter.get("/getMyShop", isAuth, getMyShop);
+
+// ✅ GET SHOPS BY CITY
 shoprouter.get("/getShopByCity/:city", getShopByCity);
-
-shoprouter.get("/getShopByCity/:city", (req, res, next) => {
-    console.log("getShopByCity Route Triggered for:", req.params.city);
-    next();
-}, getShopByCity);
 
 module.exports = shoprouter;
